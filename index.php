0e4a3438473032504158880e7806812c0b786ee6
Sonyesplay <66852843+AndresMauricioBanguero@users.noreply.github.com>
2020-08-17 19:32:34 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    // phpinfo();
+    echo "hola","mundo","PHP";
+    print "Hola mundo";
+    echo "<p>esto es php</p>";
+    echo "<h1>banguero papasito</h1>";
+    echo "<br><br>";
+    echo "=======================================<br>";
+    echo "operadores aritmeticos<br>";
+    echo "=======================================<br>";
+    echo "<br><br>";
+
+    $x=1;
+    $y=2;
+    $z=$x + $y;
+    echo $x . " + " . $y . " = " . $z . "<br>";
+    $z=$x - $y;
+    echo $x . " - " . $y . " = " . $z . "<br>";
+    $z=$x * $y;
+    echo $x . " * " . $y . " = " . $z . "<br>";
+    $z=$x / $y;
+    echo $x . " / " . $y . " = " . $z . "<br>";
+    echo "<br><br>";
+    echo "=======================================<br>";
+    echo "operadores relacionales<br>";
+    echo "=======================================<br>";
+    echo "<br><br>";
+    $x=1;
+    $y=2;
+    echo ($x > $y) , "<br>";
+    echo ($x < $y) , "<br>";
+    echo ($x == $y) , "<br>";
+    echo ($x == 1) , "<br>";
+    echo ($x ==="1") , "<br>";
+    echo ($x == "1") , "<br>";
+
+
+
+
+
+    ?>
+</body>
+</html>
